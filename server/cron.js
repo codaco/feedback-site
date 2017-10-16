b7e5bf5f56a841cc5df1924b85f5647151a5a2e0
Matt Meshulam <matt@meshul.am>
2017-10-15 21:50:47 -0500

    Auto-remove guest users older than 1 day

---
+++ server/cron.js
@@ -0,0 +1,29 @@
+import { SyncedCron } from 'meteor/percolatestudio:synced-cron';
+import moment from 'moment';
+import Accounts from 'meteor/vulcan:accounts';
+
+SyncedCron.options = {
+  log: false,
+  collectionName: 'cronHistory',
+  utc: false,
+  collectionTTL: 172800
+};
+
+const addJob = function () {
+  SyncedCron.add({
+    name: 'cleanupGuestUsers',
+    schedule(parser) {
+      return parser.text('every 5 minutes');
+    },
+    job() {
+      console.log("Removing guests older than 1 day");
+      const before = moment().subtract(1, 'days').toDate();
+      Accounts.removeOldGuests(before);
+    }
+  });
+};
+
+Meteor.startup(function () {
+  addJob();
+});
+
--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
 import '/imports/main-modules';
 import { getSetting } from 'meteor/vulcan:core';
+import './cron';
 
 Accounts.emailTemplates.from = getSetting('defaultEmail');
