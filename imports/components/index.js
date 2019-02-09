fef797174f85bad3ebf9698499f0bd48c71ab5db
Marlo Longley <marlo@codeandcursor.com>
2019-02-08 21:28:49 -0800

    WIP index importing (remove duplicate components loaded by example-forum by default)

--- imports/components/index.js
+++ imports/components/index.js
@@ -1,65 +1,73 @@
 // common
 
-import './common/Layout.jsx';
-import './common/Header.jsx';
-import './common/Logo.jsx';
-import './common/Flash.jsx';
-import './common/FlashMessages.jsx';
-import './common/SearchForm.jsx';
-import './common/Vote.jsx';
+import './common/CustomFooter.jsx';
+import './common/CustomHeader.jsx';
+import './common/CustomLogo.jsx';
+// import './common/Flash.jsx';
+// import './common/FlashMessages.jsx';
+// import './common/SearchForm.jsx';
+// import './common/Vote.jsx';
 
 // account/login
 
-import './account/SimpleLoginForm.jsx';
-import './account/SimpleSignupForm.jsx';
-import './account/SwitchableLoginForm.jsx';
-import './account/GuestLoginLink.jsx';
+import './accounts/SimpleLoginForm.jsx';
+import './accounts/SimpleSignupForm.jsx';
+import './accounts/SwitchableLoginForm.jsx';
+import './accounts/GuestLoginLink.jsx';
 
 // posts
-
+import './posts/SuggestionList.jsx';
 import './posts/SuggestedPosts.jsx';
 import './posts/CreatePostFlow.jsx';
-import './posts/PostsNewButton.jsx';
-import './posts/PostsLoadMore.jsx';
-import './posts/PostsNoMore.jsx';
-import './posts/PostsNoResults.jsx';
-import './posts/PostsItem.jsx';
-import './posts/PostsLoading.jsx';
-import './posts/PostsViews.jsx';
-import './posts/PostsList.jsx';
-import './posts/PostsListHeader.jsx';
-import './posts/PostsCategories.jsx';
-import './posts/PostsCommenters.jsx';
-import './posts/PostsPage.jsx';
-import './posts/PostsStats.jsx';
-import './posts/PostsEditForm.jsx';
-import './posts/PostsNewForm.jsx';
-import './posts/PostsCommentsThread.jsx';
+import './posts/CustomPostsNewButton.jsx';
+// import './posts/PostsLoadMore.jsx';
+// import './posts/PostsNoMore.jsx';
+// import './posts/PostsNoResults.jsx';
+// import './posts/PostsItem.jsx';
+// import './posts/PostsLoading.jsx';
+// import './posts/PostsViews.jsx';
+// import './posts/PostsList.jsx';
+// import './posts/PostsListHeader.jsx';
+// import './posts/PostsCategories.jsx';
+// import './posts/PostsCommenters.jsx';
+// import './posts/PostsPage.jsx';
+// import './posts/PostsStats.jsx';
+// import './posts/PostsEditForm.jsx';
+// import './posts/PostsNewForm.jsx';
+// import './posts/PostsCommentsThread.jsx';
+import './posts/CreatePostFlow.jsx';
+import './posts/CustomPostCommenters';
+import './posts/CustomPostsItem';
+import './posts/CustomPostsListHeader';
+import './posts/CustomPostsNewForm';
+
 
 // comments
 
-import './comments/CommentsItem.jsx';
-import './comments/CommentsList.jsx';
-import './comments/CommentsNode.jsx';
-import './comments/CommentsNewForm.jsx';
-import './comments/CommentsEditForm.jsx';
-import './comments/CommentsLoadMore.jsx';
+// import './comments/CommentsItem.jsx';
+// import './comments/CommentsList.jsx';
+// import './comments/CommentsNode.jsx';
+// import './comments/CommentsNewForm.jsx';
+// import './comments/CommentsEditForm.jsx';
+// import './comments/CommentsLoadMore.jsx';
+import './comments/CustomCommentsItem.jsx';
+
 
 // categories
 
-import './categories/CategoriesList.jsx';
-import './categories/CategoriesNode.jsx';
-import './categories/Category.jsx';
-import './categories/CategoriesEditForm.jsx';
-import './categories/CategoriesNewForm.jsx';
+// import './categories/CategoriesList.jsx';
+// import './categories/CategoriesNode.jsx';
+// import './categories/Category.jsx';
+// import './categories/CategoriesEditForm.jsx';
+// import './categories/CategoriesNewForm.jsx';
 
 // users
 
 import './users/UsersAccountStatus.jsx';
-import './users/UsersEditForm.jsx';
-import './users/UsersProfile.jsx';
-import './users/UsersProfileCheck.jsx';
-import './users/UsersAvatar.jsx';
-import './users/UsersName.jsx';
-import './users/UsersMenu.jsx';
-import './users/UsersAccountMenu.jsx';
+// import './users/CustomUsersMenu.jsx';
+// import './users/UsersProfile.jsx';
+// import './users/UsersProfileCheck.jsx';
+// import './users/UsersAvatar.jsx';
+// import './users/UsersName.jsx';
+// import './users/UsersMenu.jsx';
+// import './users/UsersAccountMenu.jsx';
