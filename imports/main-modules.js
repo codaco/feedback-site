8038ec99d548bf3c96e7b9f99c5d003782f919d3
Marlo Longley <marlo@codeandcursor.com>
2019-02-08 21:18:09 -0800

    Migrated over custom strings

--- imports/main-modules.js
+++ imports/main-modules.js
@@ -1,6 +1,7 @@
+import './routes.js';
 import './schema.js';
 import './fragments.js';
 import './components/index.js';
 import './callbacks.js';
 import './config.js';
-import './routes.js';
+import './i18n.js';
