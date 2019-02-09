2ffc8b48c255bfac4595bed335c6fed60f3c1f63
Marlo Longley <marlo@codeandcursor.com>
2019-02-08 21:11:52 -0800

    Add custom artwells guest accounts package (w previous custom code by Matt M.)

---
+++ packages/artwells-accounts-guest-custom/common.js
@@ -0,0 +1,49 @@
+import { extendFragment, addCallback } from 'meteor/vulcan:lib';
+import Users from 'meteor/vulcan:users';
+
+export const AccountsGuest = {
+  forced: true,   /* default to making loginVisitor automatic, and on logout */
+  enabled: true,  /* on 'false'  Meteor.loginVisitor() will fail */
+  //name: false,    /* defaults to returning "null" for user's name */
+  anonymous: false, /* defaults to using guests with randomly generated usernames/emails */
+};
+
+Users.addField({
+  fieldName: 'isGuest',
+  fieldSchema: {
+    type: Boolean,
+    label: 'Guest user',
+    control: 'checkbox',
+    optional: true,
+    insertableBy: ['admins'],
+    editableBy: ['admins'],
+    viewableBy: ['guests'],
+  }
+})
+
+extendFragment('UsersCurrent', `
+  isGuest
+`);
+
+function setupGuestUser(user, options) {
+  if (user.profile && user.profile.guest) {
+    user.isGuest = true;
+    user.groups = user.groups || [];
+    user.groups.push('guests');
+  } else {
+    user.isGuest = false;
+  }
+
+  return user;
+}
+
+addCallback('users.new.sync', setupGuestUser);
+
+Users.isGuest = function(userOrId) {
+  try {
+    const user = Users.getUser(userOrId);
+    return !user || !!user.isGuest || !user._id || user.username.startsWith('guest');
+  } catch (e) {
+    return true;  // Return true if not logged in
+  }
+}
