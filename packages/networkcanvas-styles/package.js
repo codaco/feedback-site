6a774de770141fa8695f6e9a9b983049ec4620fb
Marlo Longley <marlo@codeandcursor.com>
2019-02-21 12:32:43 -0800

    Linking scss importing in custom styles packages

--- packages/networkcanvas-styles/package.js
+++ packages/networkcanvas-styles/package.js
@@ -9,8 +9,8 @@ Package.onUse(function (api) {
   api.versionsFrom(['METEOR@1.0']);
 
   api.use([
-    'vulcan:core@1.7.0',
-    'fourseven:scss@4.5.0',
+    'vulcan:core',
+    'fourseven:scss@4.10.0',
   ]);
 
   api.addFiles([
